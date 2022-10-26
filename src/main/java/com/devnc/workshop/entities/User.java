befadbbe9521e2febc90f8f1af7be2abf1b04ba4
Elyson Vinicius <elysonvinicius@outlook.com>
2022-10-26 16:42:40 -0300

    Referenciando os post dos usuarios

--- src/main/java/com/devnc/workshop/entities/User.java
+++ src/main/java/com/devnc/workshop/entities/User.java
@@ -1,13 +1,15 @@
 package com.devnc.workshop.entities;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
 @Document(collection = "user")
 public class User implements Serializable {
@@ -15,7 +17,12 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
-
-
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
